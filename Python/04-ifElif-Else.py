ba29f7080bdc531f186bd5696d187c1f25652c2b
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-11-02 20:44:37 +0300

    2.11.23

---
+++ Python/04-ifElif-Else.py
@@ -0,0 +1,13 @@
+sayi1=10
+sayi2=20
+
+    
+    
+
+
+if sayi1 > sayi2:
+        print("sayi1 sayi2 den buyuktur")
+        
+print("Burasi ifin disindandir") 
+
+
---
+++ Python/05-List.py
@@ -0,0 +1,9 @@
+myList=["Yusuf",39,True,3.9]
+
+print(myList)
+print(type(myList))
+
+print(type(myList[0]))
+print(type(myList[1]))
+print(type(myList[2]))
+print(type(myList[3]))
