7f85a19fb3c8570b2c97e587f76918e432b8a8e1
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-11-06 12:46:44 +0300

    6.11.23

---
+++ Python/Day1/06-Tuple.py
@@ -0,0 +1,8 @@
+myTuple=("Yusuf","39",["Tuple icindeki liste 1","Tuple icindeki liste 2"])
+print(type(myTuple)) 
+
+#tuple larda yeni bir eleman eklenemez, var olan bir eleman güncellenemez, silinemez
+#bos bir tuple tanımlanıp daha sonra icine eleman eklenmiyor, tanımlarken degeri verilir
+#genelde library import larda kullanilir.
+
+yeniListe=list(myTuple)  #icindeki elemanları kullanabilmek icin onu donusturebiliriz
---
+++ Python/Day1/07-Dictionary.py
@@ -0,0 +1,9 @@
+#KeyValue lardan olusan 
+
+myDictionary={
+    "name":"Yusuf",
+    "Yas": 39,
+    "durum":True
+}
+
+print(myDictionary)
