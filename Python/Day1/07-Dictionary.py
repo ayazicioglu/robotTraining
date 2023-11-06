108db4724fc1b683522eeff5ab2e69a9039f02d5
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-11-06 12:51:14 +0300

    6.11.23

--- Python/Day1/07-Dictionary.py
+++ Python/Day1/07-Dictionary.py
@@ -6,4 +6,21 @@ myDictionary={
     "durum":True
 }
 
+myDictionary["yeniKey"]="Bu da degeri"   #list e yeni bir key value degeri eklemek icin
 print(myDictionary)
+
+
+
+insanlar={
+    "id1",{
+        "name":"Yusuf",
+        "yas":39,
+        "phone":["1232312312","233123123"]
+    },
+    "id2",{
+        "name":"Ahmet",
+        "yas":40
+    }
+}
+
+print(insanlar)
