98590d0d6bfb118370ec828f0bcaf840430da21d
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-11-06 12:36:24 +0300

    6.11.23

--- Python/05-List.py
+++ Python/05-List.py
@@ -7,3 +7,28 @@ print(type(myList[0]))
 print(type(myList[1]))
 print(type(myList[2]))
 print(type(myList[3]))
+
+
+myList.append("wise")
+myList.append(29)
+myList.append(False)
+print(myList)
+
+
+if 3.9 in myList:
+    print("evet")
+else:
+    print("hayir")    
+    
+    
+nums=[1,10,5,8]
+print(sorted(nums))
+
+del myList[2]
+print(myList)
+
+yeniListe= list(range(10,50))
+print(yeniListe)
+urunler=["Bilgisayar","Zil","Laptop","Monitor"]
+
+print(sorted(urunler))
