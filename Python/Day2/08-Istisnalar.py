b919e90d65939d17707f39e0661da0ff733b4f28
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-11-09 17:49:02 +0300

    9.11.23

--- Python/Day2/08-Istisnalar.py
+++ Python/Day2/08-Istisnalar.py
@@ -1,16 +1,20 @@
-sayi1= input("Lütfen bir sayi gir")
-sayi2= input("Lütfen bir sayi daha gir")
+import random
+from random import randint
+#sayi1 = input("Lutfen bir sayi girin: ")
+#sayi2 = input("Lutfen bir sayi daha girin: ")
 
-print(int(sayi1)+int(sayi2))
-
+#print(int(sayi1) + int(sayi2))
+"""
 while True:
     try:
-        sayi1= input("Lütfen bir sayi gir")
-        sayi2= input("Lütfen bir sayi daha gir")
-    
-        print(int(sayi1)+int(sayi2))
-    
+        sayi1 = int(input("Lutfen bir sayi girin: "))
+        sayi2 = int(input("Lutfen bir sayi daha girin: "))
+
+        print(sayi1 + sayi2)
     except:
-        print("Sayi dedik sayi")
+        print("Lutfen sayi girin!")
     else:
-        break        
+        break
+"""
+print(randint(0, 1))
+
