2e80e765a8173c06718d9a17a9a8a46d26a8a53b
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-11-16 20:13:10 +0300

    16.11.23

--- Python/Day2/09-Classlar.py
+++ Python/Day2/09-Classlar.py
@@ -1,20 +1,38 @@
-import random
-from random import randint
-#sayi1 = input("Lutfen bir sayi girin: ")
-#sayi2 = input("Lutfen bir sayi daha girin: ")
+class Insan():
+    def __init__(self, isim :str, soyisim :str, yas : int):
+        self.isim = isim
+        self.soyisim = soyisim
+        self.yas = yas
 
-#print(int(sayi1) + int(sayi2))
-"""
-while True:
-    try:
-        sayi1 = int(input("Lutfen bir sayi girin: "))
-        sayi2 = int(input("Lutfen bir sayi daha girin: "))
+    def bilgileriYaz(self):
+        print(f"Kullanicinin adi soyadi: {self.isim} {self.soyisim}. Yasi: {self.yas}")
+    
+    def __str__(self):
+        return f"{self.isim} {self.soyisim} {self.yas}"
+    
 
-        print(sayi1 + sayi2)
-    except:
-        print("Lutfen sayi girin!")
-    else:
-        break
-"""
-print(randint(0, 1))
+    def __len__(self):
+        return self.yas
 
+
+insan = Insan("Yusuf", "Yusufoglu", 39)
+
+insan.bilgileriYaz()
+
+print(str(insan))
+
+print(len(insan))
+
+
+class Yusuf(Insan):
+    def __init__(self, isim, soyisim, yas):
+        super().__init__(isim, soyisim, yas)
+
+    def ysfFonk(self):
+        print("Bu method yusufa ait")
+
+
+ysf = Yusuf("AAA", "BBBB", 10)
+
+ysf.ysfFonk()
+ysf.bilgileriYaz()
